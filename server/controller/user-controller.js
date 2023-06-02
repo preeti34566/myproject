d33aecd73175c5713b021a023717a114d26abe1d
Preeti <sharmaaglina@gmail.com>
2023-06-02 12:48:19 +0530

    commit

---
+++ server/controller/user-controller.js
@@ -0,0 +1,16 @@
+import User from '../model/user.js';
+
+
+// Save data of the user in database
+export const addUser = async (request, response) => {
+    const user = request.body;
+    
+    const newUser = new User(user);
+    try{
+        await newUser.save();
+        response.status(201).json(newUser);
+    } catch (error){
+        response.status(409).json({ message: error.message});     
+    }
+}
+
